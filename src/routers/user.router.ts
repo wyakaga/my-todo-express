1119680acdb9947c080c906b5217c63156b36b5f
wyakaga <123617005+wyakaga@users.noreply.github.com>
2023-10-22 13:27:29 +0700

    [FEAT] Add middleware to update user data router

--- src/routers/user.router.ts
+++ src/routers/user.router.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 
 import userController from "../controllers/user.controller";
+import isAuthenticated from "../middlewares/isAuthenticated";
 
 const userRouter: Router = Router();
 
-userRouter.get("/api/v1/user/:id", userController.readSingle);
-userRouter.patch("/api/v1/user/:id", userController.update);
+userRouter.get("/api/v1/user/:id", isAuthenticated, userController.readSingle);
+userRouter.patch("/api/v1/user/:id", isAuthenticated, userController.update);
 
 export default userRouter;
