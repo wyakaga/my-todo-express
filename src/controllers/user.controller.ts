71509b080c9e99e98ed0dcf9f0b265b5e7cf129b
wyakaga <123617005+wyakaga@users.noreply.github.com>
2023-10-22 13:08:28 +0700

    [FEAT] Add update user data router

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -24,4 +24,36 @@ export default {
 			response(res, 500, "Internal Server Error", (error as Error).message);
 		}
 	},
+	update: async (req: Request, res: Response) => {
+		try {
+			const id = Number(req.params.id);
+			const { firstName, lastName }: { firstName: string; lastName: string } = req.body;
+
+			const checkUser = await prisma.user.findUnique({ where: { id } });
+			if (!checkUser) return response(res, 400, "No such user exist!");
+
+			const updatedData = {
+				firstName: firstName || checkUser.firstName,
+				lastName: lastName || checkUser.lastName,
+			};
+
+			const data = await prisma.user.update({
+				where: { id },
+				select: {
+					id: true,
+					email: true,
+					firstName: true,
+					lastName: true,
+					createdAt: true,
+					updatedAt: true,
+				},
+				data: updatedData,
+			});
+
+			response(res, 200, "Updated succesfully!", data);
+		} catch (error) {
+			console.log(error);
+			response(res, 500, "Internal Server Error", (error as Error).message);
+		}
+	},
 };
--- src/routers/user.router.ts
+++ src/routers/user.router.ts
@@ -5,5 +5,6 @@ import userController from "../controllers/user.controller";
 const userRouter: Router = Router();
 
 userRouter.get("/api/v1/user/:id", userController.readSingle);
+userRouter.patch("/api/v1/user/:id", userController.update);
 
 export default userRouter;
